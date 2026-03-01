a119928e81a44fa25d6ea19351d2dd43fdab58ce
deeprajkumargupta <192437937+deeprajkumargupta@users.noreply.github.com>
2026-03-01 18:33:01 +0530

    setup redux store and basic configuration, create cart slice

--- src/main.jsx
+++ src/main.jsx
@@ -2,42 +2,46 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
-import Products from "./pages/Products"
-import ProductDetails from "./pages/ProductDetails"
-import NotFound from "./pages/NotFound"
-import Cart from "./pages/Cart"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
 import CartProvider from "./context/CartContext";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:<App />,
-    children:[
+    element: <App />,
+    children: [
       {
         index: true,
-        element:<Products/>
+        element: <Products />,
       },
       {
         path: "product/:id",
-        element:<ProductDetails/>
+        element: <ProductDetails />,
       },
       {
         path: "cart",
-        element: <Cart/>
-      }
-    ]
+        element: <Cart />,
+      },
+    ],
   },
   {
     path: "*",
-    element:<NotFound/>,
+    element: <NotFound />,
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CartProvider>
-      <RouterProvider router={router}/>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </CartProvider>
   </StrictMode>,
 );
